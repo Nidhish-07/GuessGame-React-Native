a6ffe6cfc982d645f3dfe6a7fc7fcdfcd2cddb98
Nidhish Chauhan <rch246chauhan@gmail.com>
2022-12-07 19:06:27 +0530

    Initial Commit

---
+++ screens/StartScreen.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View,TextInput,Button } from 'react-native'
+import React from 'react'
+
+type Props = {}
+
+const StartScreen = (props: Props) => {
+  return (
+    <View>
+      <Text>StartScreen</Text>
+    </View>
+  )
+}
+
+export default StartScreen
+
+const styles = StyleSheet.create({})
